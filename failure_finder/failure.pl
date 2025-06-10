fcd18e5989b2f90471c81c12af711a15e8cf94b9
kostas-stathis <kostas.stathis@rhul.ac.uk>
2025-06-10 22:40:46 +0100

    messed up failure version

--- failure_finder/failure.pl
+++ failure_finder/failure.pl
@@ -1,15 +1,43 @@
-find_failure(Goal, undefined(Goal)):-
-    undefined(Goal), !.
+/* 
 
-find_failure(Goal, failed(Goal)) :-
-     defined(Goal), !,
-    \+ call(Goal).
+Author: Kostas Stathis
+Date: 2023-10-01
+This file contains the implementation of the find_failure/2 predicate.
+This predicate is used to find failures in a Prolog goal.
+Usage:
+?- find_failure(p(X), Failure).
+*/
 
-find_failure(Goal, Failure) :-
+% This file is part of the Failure Finder library.
+goal_failure(Goal, ):-
+    \+ defined(Goal), !.
+
+find_failure(Goal, Goal:(Succeeded, Failed, Untried)) :-
+    defined(Goal), !,
     clause(Goal, Body),
-    find_failures(Body, BodyFailures).
+    find_failures(Body, Succeeded, Failed, Untried).
 
-find_failures([], BodyFailures) :-!.
-find_failures([Head|Tail], [HeadFailure|TailFailures]) :-
+/* I think it is important to have the following
+
+Head: Succeeded, Failure, Untried.
+
+For Failure we need the deep failure.
+
+*/
+
+find_failures(true, [true], [], []) :- !.
+
+find_failures((Head,Tail), [Head|Rest], F, Untried) :-
+    defined(Head),
     find_failure(Head, HeadFailure),
     find_failures(Tail, TailFailures).
+
+defined(Goal) :-
+    functor(Goal, Functor, Arity),
+    current_predicate(Functor/Arity).
+
+
+p(X):- q(X), r(X).
+
+q(a).
+q(b).
